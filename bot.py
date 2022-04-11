47e5a833d8c06ffe6be19972d8ef162fb34b3575
FHieser <47272854+FHieser@users.noreply.github.com>
2022-04-11 23:09:04 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 import info
 
@@ -14,19 +15,99 @@ async def setup(ctx):
     guild = bot.guilds[0]
     await ctx.send('Setup start:')
     
+    
+    
+    #Remove unnecessary roles
+    await ctx.send('Removing unnecessary roles:')
+
+    for role in guild.roles:
+        if role.name!='BuilderBot' and role.id!=guild.default_role.id:
+            await role.delete()
+            await ctx.send("Deleted Role: {}".format(role.name))
+
+    #Create Roles
+    await ctx.send('Creating Roles')
+    #Owner
+    owner = await guild.create_role(name="Owner",colour=discord.Colour.dark_gold())
+    
+    #Admin
+    admin = await guild.create_role(name="Admin",colour=discord.Colour.red())
+    #Moderator
+    moderator = await guild.create_role(name="Moderator",colour=discord.Colour.dark_green())
+    #Member
+    member = await guild.create_role(name="Member",colour=discord.Colour.blue())
+    
+    
+    #Setup Permissions for Roles
+    defaultPerms = discord.Permissions.none()
+
+    memberPerms=defaultPerms
+    memberPerms.update(view_channel = True)
+    memberPerms.update(create_instant_invite = True)
+    memberPerms.update(send_messages = True)
+    memberPerms.update(add_reactions = True)
+    memberPerms.update(use_external_emojis = True)
+    memberPerms.update(external_stickers = True)
+    memberPerms.update(external_stickers = True)
+    memberPerms.update(read_message_history = True)
+    memberPerms.update(connect = True)
+    memberPerms.update(speak = True)
+
+    moderatorPerms=memberPerms
+    moderatorPerms.update(manage_emojis = True)
+    moderatorPerms.update(view_audit_log =True)
+    moderatorPerms.update(view_guild_insights =True)
+    moderatorPerms.update(change_nickname = True)
+    moderatorPerms.update(manage_nicknames = True)
+    moderatorPerms.update(embed_links = True)
+    moderatorPerms.update(attach_files = True)
+    moderatorPerms.update(mention_everyone = True)
+    moderatorPerms.update(manage_messages = True)
+    moderatorPerms.update(use_slash_commands = True)
+    moderatorPerms.update(stream = True)
+    moderatorPerms.update(mute_members = True)
+    moderatorPerms.update(deafen_members = True)
+    moderatorPerms.update(move_members = True)
+
+    adminPerms=moderatorPerms
+    adminPerms.update(manage_channels = True)
+    adminPerms.update(manage_roles = True)
+    adminPerms.update(manage_webhooks = True)
+    adminPerms.update(ban_members = True)
+    adminPerms.update(kick_members = True)
+        
+    ownerPerms=adminPerms
+    ownerPerms.update(manage_guild = True)
+
+    #Edit Roles
+    await ctx.send('Editing Role Permissions')
+    await owner.edit(permissions=ownerPerms)
+    await admin.edit(permissions=adminPerms)
+    await moderator.edit(permissions=moderatorPerms)
+    await member.edit(permissions=memberPerms)
+    await guild.default_role.edit(permissions=defaultPerms)
+
+
+    #Remove unneccessary channels
     await ctx.send('Removing unneccessary channels:')
     for channel in guild.text_channels:
         if channel.name!='welcome':
             await channel.delete()
 
+    #Remove unneccessary categories
     await ctx.send('Removing unneccessary categories:')
     for category in guild.categories:
         await category.delete()
 
+    #Creating Categories and Channels
+    await ctx.send('Creating Categories and Channels')
+    
     #Server Stats
     category = await guild.create_category('______SERVER STATS______')
     await guild.create_text_channel('|available', category=category)
     await guild.create_text_channel('|members', category=category)
+
+    #Mod Channels
 
     #Important
     category = await guild.create_category('_______IMPORTANT________')
